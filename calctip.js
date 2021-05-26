60d894c6ae37cf9f591ff123db8538906e58adb1
AmanuelChorito <amye.end@gmail.com>
2021-05-25 21:41:20 -0500

    p

--- calctip.js
+++ calctip.js
@@ -1,10 +1,10 @@
-document.querySelector("#calcform").addEventListener("submit", function (e) {
+document.querySelector(alcform).addEventListener("submit", function (e) {
   e.preventDefault();
-  var subtotalElem = document.getElementById("#subtotal").value;
-  var tipElem = document.getElementById("#tip").value;
-  var totalElem = document.getElementById("#total");
+  var subtotalElem = document.getElementById("subtotal").value;
+  var tipElem = document.getElementById("tip").value;
+  var totalElem = document.getElementById("total");
   var subtotal = parseFloat(subtotalElem);
   var tip = (parseFloat(tipElem) / 100) * subtotal;
   var total = subtotal + tip;
-  document.getElementById("#total").innerHTML = "$" + total;
+  document.getElementById("total").innerHTML = "$" + total;
 });
