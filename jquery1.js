3e31bbbdbf6b15fcf379f60ec30905270acf6ae4
AmanuelChorito <amye.end@gmail.com>
2021-05-30 17:14:38 -0500

    p

--- jquery1.js
+++ jquery1.js
@@ -1,4 +1,4 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>;
 $(document).ready(function () {
-  $("div h1").css("bacground-color", "red");
+  $("div h1").css("background-color", "red");
 });
