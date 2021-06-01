db1f32b7b1d9f15ed0d221684ea4d7169493a43e
AmanuelChorito <amye.end@gmail.com>
2021-06-01 10:07:56 -0500

    p

--- jquery2.js
+++ jquery2.js
@@ -1,4 +1,4 @@
 $(document).ready(function () {
-  var msg = " <div><h1>" + JQuery + " " + Core + "%</h1></div>";
+  var msg = "<div><h1>" + JQuery + " " + Core + "</h1></div>";
   $("body").append(msg);
 });
