8f874ad13a2a17f13f2253ba9cdea2ebe8a91ba7
AmanuelChorito <amye.end@gmail.com>
2021-06-02 17:41:10 -0500

    p

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
   $("ul.level-2").children().css("background-color", "red");
+
   $(".third-item").prevAll().css("background-color", "blue");
   $("span").parent().css("background-color", "green");
+
+  var t = $("body").children().first().text();
+  $("body").children().eq(1).text(t);
 });
