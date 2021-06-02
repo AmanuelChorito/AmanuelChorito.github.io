ea51f84807798c68c889de21451949063fa14cfa
AmanuelChorito <amye.end@gmail.com>
2021-06-02 18:13:38 -0500

    p

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 $(document).ready(function () {
-  $("ul.level-2").children().css("background-color", "red");
+  var de = $("ul.level-2")
+    .children()
+    .not($("ul.level-3"))
+    .css("background-color", "blue");
 
-  $(".third-item").prevAll().css("background-color", "blue");
+  var x = $("ul.level-3").detach();
+
+  //var l2 = $("ul.level-2")
+  //  .children()
+
+  // var l1 = l2.prev();
+  // var l3 = l2.next();
+
+  // l1.css("background-color", "blue");
+  // l2.css("background-color", "blue");
+  // l3.css("background-color", "blue");
+
+  $(".third-item").prev().css("background-color", "blue");
   $("span").parent().css("background-color", "green");
 
   var t = $("body").children().first().text();
