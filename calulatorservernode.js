1b60fb01e855949f6b28d5526ddb1d5380edc454
AmanuelChorito <amye.end@gmail.com>
2021-06-04 07:28:51 -0500

    p

--- calulatorservernode.js
+++ calulatorservernode.js
@@ -1,21 +1,10 @@
 var http = require("http");
 const { URL } = require("url");
 var fs = require("fs");
-var addmod = require("./addmod.js");
+var calculate = require("./operationoutput.js");
 http
   .createServer(function (req, res) {
     var q = new URL(req.url, "https://localhost:8085/");
-    var filename = "." + q.pathname;
-    if (q.pathname == "/add.js") addmod.add(req, res, q.searchParams);
-    else
-      fs.readFile(filename, function (err, data) {
-        if (err) {
-          res.writeHead(404, { "Content-Type": "text/html" });
-          return res.end("404 Not Found");
-        }
-        res.writeHead(200); // Content-Type not included
-        res.write(data);
-        return res.end();
-      });
+    calculate.result(req, res, q.searchParams);
   })
   .listen(8085);
--- operationoutput.js
+++ operationoutput.js
@@ -1,5 +1,8 @@
-exports.add = function (req, res, vals) {
-  var sum = parseInt(vals.get("first")) + parseInt(vals.get("second"));
+exports.result = function (req, res, vals) {
+  let num1 = parseInt(vals.get("number1"));
+  let num2 = parseInt(vals.get("number2"));
+  let selectedOperation = vals.get("operation");
+  output = calc(num1, num2, selectedOperation);
   res.writeHead(200, { "Content-Type": "text/html" });
   res.write("<!DOCTYPE html>");
   res.write("<html>");
@@ -7,10 +10,27 @@ exports.add = function (req, res, vals) {
   res.write("<title>Calculator Web Site</title>");
   res.write("</head>");
   res.write("<body>");
-  res.write("<p>The sum is: ");
-  res.write(String(sum));
+  res.write("<p>The Answer is : ");
+  res.write(String(output));
   res.write("</p>");
+  res.write(
+    "<a href='https://amanuelchorito.github.io/SimpleCalculator.html'>Go Back</a>"
+  );
   res.write("</body>");
   res.write("</html>");
   return res.end();
 };
+function calc(num1, num2, selectedOperation) {
+  switch (selectedOperation) {
+    case "Add":
+      return num1 + num2;
+
+    case "Multiply":
+      return num1 * num2;
+    case "Substract":
+      return num1 - num2;
+    case "Divide":
+      return num1 * num2;
+    default:
+  }
+}
