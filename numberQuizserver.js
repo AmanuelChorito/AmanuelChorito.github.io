8a53df4d74a64b938a723ba17d030f3079f02a5d
AmanuelChorito <amye.end@gmail.com>
2021-06-05 11:21:59 -0500

    p

--- numberQuizserver.js
+++ numberQuizserver.js
@@ -4,7 +4,16 @@ var fs = require("fs");
 var output = require("./numberQuiz.js");
 http
   .createServer(function (req, res) {
-    var q = new URL(req.url, "https://localhost:8085/");
-    output.guessnumber(req, res, q.searchParams);
+    if (req.url == "/quiz") {
+      var q = new URL(req.url, "https://localhost:8085/");
+      output.guessnumber(req, res, q.searchParams);
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(
+      '<form action="startquiz" method="post" enctype="multipart/form-data">'
+    );
+    res.write('<input type="submit" value="Start">');
+    res.write("</form>");
+    return res.end();
   })
   .listen(8085);
